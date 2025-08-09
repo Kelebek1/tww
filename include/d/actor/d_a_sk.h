e94850cbcbcc1f0f98d70b4ffc0c38fcb345df44
Kelebek1 <eeeedddccc@hotmail.co.uk>
2025-08-09 18:44:12 +0100

    d_a_sk

--- include/d/actor/d_a_sk.h
+++ include/d/actor/d_a_sk.h
@@ -2,10 +2,29 @@
 #define D_A_SK_H
 
 #include "f_op/f_op_actor.h"
+#include "d/d_cc_d.h"
 
-class sk_class : public fopAc_ac_c {
-public:
-    /* Place member variables here */
-};
+class mDoExt_McaMorf;
+
+struct sk_class {
+    /* 0x000 */ fopAc_ac_c actor;
+    /* 0x290 */ u8 m290[0x2AC - 0x290];
+    /* 0x2AC */ request_of_phase_process_class mPhase;
+    /* 0x2B4 */ u8 m2B4;
+    /* 0x2B5 */ u8 m2B5;
+    /* 0x2B8 */ mDoExt_McaMorf* mpMorf;
+    /* 0x2BC */ bool m2BC;
+    /* 0x2BE */ s16 m2BE;
+#if VERSION > VERSION_DEMO
+    /* 0x2C0 */ s16 m2C0;
+#endif
+    /* 0x2C2 */ csXyz m2C2[4];
+    /* 0x2DC */ cXyz m2DC[4];
+    /* 0x30C */ dCcD_Stts mStts;
+    /* 0x348 */ dCcD_Sph m348[5];
+#if VERSION > VERSION_DEMO
+    /* 0x924 */ JntHit_c* m924;
+#endif
+}; // size = 0x928
 
 #endif /* D_A_SK_H */
